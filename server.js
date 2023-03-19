0c721527d4584acdab470462bbabf47510b9341c
Gustavo Garcia <gustavo.garcia@oliveiratrust.com.br>
2023-03-19 16:34:29 -0300

    feat: Removido webpack e instalado vite

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-// const express = require("express");
-// const cors = require("cors");
-import path from 'path'
-import express from 'express';
-import cors from "cors";
-import webpack from 'webpack'
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from '/webpack.server.config'
+const express = require("express");
+const ViteExpress = require("vite-express");
+const cors = require('cors')
 
-const app = express(),DIST_DIR = __dirname,
-compiler = webpack(config)
+// import express from "express";
+// import cors from "cors"
+// import ViteExpress from "vite-express";
 
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
-}))
-app.use(webpackHotMiddleware(compiler))
+const app = express()
 
 var corsOptions = {
     origin: "http://localhost:8081"
@@ -24,9 +16,6 @@ require('dotenv').config({ path: './.env' })
 
 app.use(cors(corsOptions));
 
-app.use(express.static(DIST_DIR));
-
-// parse requests of content-type - application/json
 app.use(express.json());
 
 const db = require("./app/models");
@@ -38,19 +27,16 @@ db.sequelize.sync()
     console.log("Failed to sync db: " + err.message);
   });
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 // simple route
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   res.json({ message: `Welcome to ${process.env.APP_NAME} application. Version: ${process.env.APP_VERSION}` });
 });
 
 // importando as rotas
 require("./app/routes/administrador.routes")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+
+ViteExpress.listen(app, PORT, () => console.log("Server is listening..."));
